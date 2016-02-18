c99d5a9d413dedb3beee6ee3e52249d46562dd86
sameera91 <sbachoti91@gmail.com>
2016-02-18 14:18:25 -0500

    second commit

--- lib/ted_talks/cli.rb
+++ lib/ted_talks/cli.rb
@@ -4,24 +4,66 @@ class TedTalks::CLI
 	def call
 		puts "Today's TED ideas worth spreading."
 		puts " "
-		list_talks
+		list_categories
 		menu
 		goodbye
 	end
 
-	def list_talks
-		@talks = TedTalks::Talk.top_talks
+	def list_talks(url='http://www.ted.com/talks')
+		@talks = TedTalks::Talk.top_talks(url)
 		@talks.each.with_index do |talk, i|
 			puts "#{i+1}. #{talk.title} - #{talk.author} - Posted #{talk.date} - Rated #{talk.rating}"
 			puts " "
 		end
 	end
 
+	def list_categories
+		puts "Here are 7 different categories. Enter the number of the category you'd like to see more videos about or enter search to search for videos."
+		puts "1. Top Talks"
+		puts "2. Technology"
+		puts "3. Entertainment"
+		puts "4. Design"
+		puts "5. Business"
+		puts "6. Science"
+		puts "7. Global Issues"
+
+		input = gets.strip.downcase
+
+		if input == "top talks" || input == "1"
+			list_talks
+		elsif input == "technology" || input == "2"
+			url = "http://www.ted.com/talks?topics%5B%5D=technology&sort=newest"
+			list_talks(url)
+		elsif input == "entertainment" || input == "3"
+			url = 'http://www.ted.com/talks?topics%5B%5D=entertainment&sort=newest'
+			list_talks(url)
+		elsif input == "design" || input == "4"
+			url = "http://www.ted.com/talks?topics%5B%5D=design&sort=newest"
+			list_talks(url)
+		elsif input == "business" || input == "5"
+			url = "http://www.ted.com/talks?topics%5B%5D=business&sort=newest"
+			list_talks(url)
+		elsif input == "science" || input == "6"
+			url = "http://www.ted.com/talks?topics%5B%5D=science&sort=newest"
+			list_talks(url)
+		elsif input == "global issues" || input == "7"
+			url = "http://www.ted.com/talks?topics%5B%5D=global+issues&sort=newest"
+			list_talks(url)
+		elsif input == "search"
+			puts "Enter your search term."
+			search = gets.strip.downcase
+			url = "http://www.ted.com/talks?q=#{search}&sort=newest"
+			list_talks(url)
+		end
+
+	end
+
 	def menu
 		input = nil
 		while input != "exit"
 			puts " "
-			puts "Enter the number of the talk you'd like to learn more about or type list to see the articles again. Type exit to quit the program."
+			puts "Enter the number of the talk you'd like to learn more about, type list to see the articles again, or type categories to see the categories." 
+			puts "Type exit to quit the program."
 			input = gets.strip.downcase
 
 			if input.to_i > 0
@@ -33,14 +75,17 @@ class TedTalks::CLI
 				display_talk_info(doc)
 			elsif input == "list"
 				list_talks
-			else
-				puts "Invalid number. Type list or exit."
+			elsif input == "categories"
+				list_categories
+			elsif input != "exit"
+				puts "Invalid number. Type list, categories, or exit."
 			end
 		end
 	end
 
 	def display_talk_info(doc)
 
+		puts "----------------------------------------------"
 		puts doc.search("div.player-hero__speaker").search("span.player-hero__speaker__content").text
 		puts doc.search("div.player-hero__title").search("span.player-hero__title__content").text
 		puts doc.search(".talk-subsection").search(".talk-top__details").search("p.talk-description").text
@@ -49,6 +94,7 @@ class TedTalks::CLI
 		puts doc.search("div.player-hero__meta").search("span")[1].text
 		puts " "
 		puts doc.search("div.talk-sharing__count").search("span.talk-sharing__value").text.strip! + " total views"
+		puts "-----------------------------------------------"
 	end
 
 	def goodbye
--- lib/ted_talks/talk.rb
+++ lib/ted_talks/talk.rb
@@ -2,27 +2,34 @@ class TedTalks::Talk
 
 	attr_accessor :title, :author, :date, :rating, :url
 
-	def self.top_talks
+	def self.top_talks(url)
 
 		talks = []
 
-	  (0..6).each do |i|
-		talks << self.scrape_talks_ted(i)
+	  (0..4).each do |i|
+	  	talk_object = self.scrape_talks_ted(i,url)
+
+	  	if talk_object.title == nil
+	  		break
+	  	else
+			talks << self.scrape_talks_ted(i, url)
+		end
 	  end
 
 		talks
 	end
 
-	def self.scrape_talks_ted(i)
-		doc = Nokogiri::HTML(open('http://www.ted.com/talks'))
+	def self.scrape_talks_ted(i, url='http://www.ted.com/talks')
+
+		doc = Nokogiri::HTML(open(url))
 		#binding.pry
 		talk = self.new
-		talk.title = doc.search(".media__message").search("h4.h9").search("a")[i].text.strip!
-		talk.author = doc.search(".media__message").search("h4.h12")[i].text
-		talk.date = doc.search(".media__message").search(".meta").search(".meta__item").search("span.meta__val")[i].text.strip!
-		talk.rating = doc.search(".media__message").search(".meta").search(".meta__row").search(".meta__val")[i].text.strip!
+		talk.title = doc.search(".media__message").search("h4.h9").search("a")[i].text.strip! if doc.search(".media__message").search("h4.h9").search("a")[i] != nil
+		talk.author = doc.search(".media__message").search("h4.h12")[i].text if doc.search(".media__message").search("h4.h12")[i] != nil
+		talk.date = doc.search(".media__message").search(".meta").search(".meta__item").search("span.meta__val")[i].text.strip! if doc.search(".media__message").search(".meta").search(".meta__item").search("span.meta__val")[i] != nil
+		talk.rating = doc.search(".media__message").search(".meta").search(".meta__row").search(".meta__val")[i].text.strip! if doc.search(".media__message").search(".meta").search(".meta__row").search(".meta__val")[i] != nil
 		#talk.category.strip!
-		talk.url = "http://www.ted.com" + doc.search("h4.h9").css("a")[i].attr("href")
+		talk.url = "http://www.ted.com" + doc.search("h4.h9").css("a")[i].attr("href") if doc.search("h4.h9").css("a")[i] != nil
 	 	talk
 	end
 end
